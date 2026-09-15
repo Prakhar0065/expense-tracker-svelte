203a0cce8953e45c2411cdca87e87cef169486e7
prakhar <prakhar0065@gmail.com>
2026-09-15 12:34:48 +0530

    Initial Svelte project

---
+++ my-first-app/src/lib/ColorDisplay.svelte
@@ -0,0 +1,8 @@
+<script>
+    let { color } = $props();
+</script>
+
+<div>
+    <h2>Your choice is:</h2>
+    <h3>{color}</h3>
+</div>
---
+++ my-first-app/src/routes/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+    import ColorDisplay from '$lib/ColorDisplay.svelte';
+
+    let selectedColor = $state("Blue");
+
+    function selectColor(color) {
+        selectedColor = color;
+    }
+</script>
+
+
+    <h1>My Preference</h1>
+
+    <p>Choose a color:</p>
+
+    <button onclick={() => selectColor("Blue")}>
+        Blue
+    </button>
+
+    <button onclick={() => selectColor("Green")}>
+        Green
+    </button>
+
+    <button onclick={() => selectColor("Red")}>
+        Red
+    </button>
+
+    <ColorDisplay color={selectedColor} />
